1d8788b8be7cc5a13e33d4762e80edd6ea0ccee5
PongPong <pongpong@example.com>
2025-06-15 23:21:00 +0900

    코드 수정

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb99fde503541f23efb398258a0d4e2f1059785b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cf94ef264fabd0564c0e1dc4b7a938b1c4fecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
